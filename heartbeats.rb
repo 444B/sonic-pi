c95e3e521b0656839c4ce9af02a3bb33d2181fc7
444B <15311579+444B@users.noreply.github.com>
2024-03-04 14:41:58 +0200

    Create heartbeats.rb

---
+++ heartbeats.rb
@@ -0,0 +1,13 @@
+# This sounds like a heart beat
+
+live_loop :flibble do
+  sample :bd_haus, rate:0.01
+  sleep 1
+end
+
+sleep 0.18
+
+live_loop :flobble do
+  sample :bd_haus, rate:0.01
+  sleep 1
+end
